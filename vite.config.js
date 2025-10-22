5ee27b1fbb71a3b73882ecb3bf2d88cac6bf63eb
Sathsarani2002 <sathsaranigeethamali2002@gmail.com>
2025-10-22 19:58:58 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173 // optional: dev server port
-  }
+    port: 5173, // optional: dev server port
+  },
+  // 👇 Add this line — very important for GitHub Pages
+  base: "/manage-doctors/",
 });
